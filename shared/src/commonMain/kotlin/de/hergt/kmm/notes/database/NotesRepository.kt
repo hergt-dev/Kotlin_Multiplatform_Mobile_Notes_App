c6d91492d524f116e3358869845614e18d47fe26
Leon Hergt <hergt@emgress.de>
2023-04-17 10:34:52 +0200

    modified data flow for ios platform

--- shared/src/commonMain/kotlin/de/hergt/kmm/notes/database/NotesRepository.kt
+++ shared/src/commonMain/kotlin/de/hergt/kmm/notes/database/NotesRepository.kt
@@ -10,12 +10,18 @@ import kotlinx.coroutines.flow.Flow
 
 class NotesRepository(private val mQueries: NotesQueries) {
 
-    fun getNotes(): Flow<List<Notes>> {
-        return mQueries.getNotes().asFlow().mapToList(Dispatchers.Default)
+    fun getNotes(): CommonFlow<List<Notes>> {
+        return mQueries.getNotes()
+            .asFlow()
+            .mapToList(Dispatchers.Default)
+            .asCommonFlow()
     }
 
-    fun getNoteById(id: Long): Flow<Notes> {
-        return mQueries.getNoteById(id).asFlow().mapToOneNotNull(Dispatchers.Default)
+    fun getNoteById(id: Long): CommonFlow<Notes> {
+        return mQueries.getNoteById(id)
+            .asFlow()
+            .mapToOneNotNull(Dispatchers.Default)
+            .asCommonFlow()
     }
 
     fun createNote(note: Notes) {
